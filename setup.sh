24296ae70a4406704245526a0ccbb38522b86a4b
feycot <feycot@gmail.com>
2022-02-04 21:43:16 +0500

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+
+sudo apt update
+sudo apt-get install -y make nodejs postgresql
+
+sudo -u postgres createuser --createdb vagrant
+sudo -u postgres createdb vagrant
+sudo -u postgres psql -c "ALTER USER vagrant WITH PASSWORD 'vagrant';"
